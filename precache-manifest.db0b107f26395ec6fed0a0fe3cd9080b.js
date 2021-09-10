c1b92fc2f34f3e10163d17eb9891bdcce9bc76bf
dn9131 <87651336+dn9131@users.noreply.github.com>
2021-09-10 17:51:12 +0530

    Updates

---
+++ precache-manifest.db0b107f26395ec6fed0a0fe3cd9080b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf4d64bd1dd09b105e4fa771b8a8f3d5",
+    "url": "/newrobofriends/index.html"
+  },
+  {
+    "revision": "97cb5fb343bd6c8ba29e",
+    "url": "/newrobofriends/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "0a27df7ef54b6f6af425",
+    "url": "/newrobofriends/static/css/main.4ebb84ac.chunk.css"
+  },
+  {
+    "revision": "97cb5fb343bd6c8ba29e",
+    "url": "/newrobofriends/static/js/2.436754dc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/newrobofriends/static/js/2.436754dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a27df7ef54b6f6af425",
+    "url": "/newrobofriends/static/js/main.fce5a2b5.chunk.js"
+  },
+  {
+    "revision": "8a45f3189b81511e73cf",
+    "url": "/newrobofriends/static/js/runtime-main.dbab8a05.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/newrobofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
